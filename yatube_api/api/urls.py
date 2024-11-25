311bbeb4ca2b5b73c290a5d471e3155f1d730502
Nokvix <igor.gass2003@mail.ru>
2024-11-25 21:33:25 +0500

    Настроил работу эндпоинта /posts/

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+from .views import PostViewSet
+
+
+router = SimpleRouter()
+
+router.register('posts', PostViewSet)
 
 urlpatterns = [
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
+    path('v1/', include(router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,11 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+from posts.models import Post, Comment, Follow
+from .serializers import PostSerializer, CommentSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
